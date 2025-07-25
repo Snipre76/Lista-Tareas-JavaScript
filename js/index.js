1d413d5035e485e328e07cf09b174fb44fcff22a
Snipre76 <dannycollazos93@gmail.com>
2025-07-25 13:01:38 -0500

    Proyecto finalizado

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ function agregarTarea() {
 
         var item = document.createElement("li"); //Creamos la etiqueta li por medio del DOM, la guardamos en una variable
 
-        item.innerHTML = '<input type="button" id="eliminar" onclick="eliminar()" value="X" />' +" "+ newTask; //Agregamos el boton y el texto a la variable item
+        item.innerHTML = '<input type="button" id="eliminar" onclick="eliminar(this.parentNode)" value="X" />' + " " + newTask; //Agregamos el boton y el texto a la variable item
         //innerHTML es una propiedad que nos permite agregar HTML a un elemento
         document.getElementById("listaTareas").appendChild(item); //Agregamos la variable item a la lista de tareas
 
@@ -19,8 +19,8 @@ function agregarTarea() {
     }
 }
 
-function eliminar() {
-    let boton = document.getElementById("eliminar");
-    let tarea = boton.parentElement; // Obtener el elemento li
-    tarea.remove();
+function eliminar(item) {
+        item.remove();
+    
+
 }
